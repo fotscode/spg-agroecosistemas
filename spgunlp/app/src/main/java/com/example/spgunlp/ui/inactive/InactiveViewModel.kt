465972820d69e08f3b61c9b05bb0fd0ff6a39c90
fots <facundotomatis@gmail.com>
2023-11-29 00:20:03 -0300

    initial commit

---
+++ spgunlp/app/src/main/java/com/example/spgunlp/ui/inactive/InactiveViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.spgunlp.ui.inactive
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class InactiveViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
