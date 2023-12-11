05f19e90d6a818b05ba290b755ecc8778e3da10f
fots <facundotomatis@gmail.com>
2023-12-11 20:39:44 -0300

    added stats fragment content

--- spgunlp/app/src/main/java/com/example/spgunlp/ui/stats/StatsFragment.kt
+++ spgunlp/app/src/main/java/com/example/spgunlp/ui/stats/StatsFragment.kt
@@ -1,15 +1,22 @@
 package com.example.spgunlp.ui.stats
 
+import android.animation.ObjectAnimator
+import android.graphics.Color
+import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.animation.doOnEnd
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.spgunlp.R
 import com.example.spgunlp.databinding.FragmentStatsBinding
+import com.example.spgunlp.ui.BaseFragment
 
-class StatsFragment : Fragment() {
+class StatsFragment : BaseFragment() {
 
     private var _binding: FragmentStatsBinding? = null
 
@@ -18,21 +25,35 @@ class StatsFragment : Fragment() {
     private val binding get() = _binding!!
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-                ViewModelProvider(this).get(StatsViewModel::class.java)
-
         _binding = FragmentStatsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textStats
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        binding.cardView.setOnClickListener() {
+            binding.cardView.invalidate()
+            ObjectAnimator.ofArgb(
+                binding.cardView,
+                "strokeColor",
+                getColor(R.color.purple_200),
+                getColor(R.color.green),
+                getColor(R.color.teal_200),
+                getColor(R.color.purple_200),
+            ).apply {
+                duration = 3000
+                start()
+                doOnEnd {
+                    reverse()
+                }
+            }
         }
         return root
+    }
+
+    private fun getColor(color: Int): Int {
+        return ContextCompat.getColor(requireContext(), color)
     }
 
     override fun onDestroyView() {
