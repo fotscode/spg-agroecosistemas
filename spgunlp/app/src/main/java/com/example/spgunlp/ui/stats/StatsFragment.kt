028feef7fc007c58dbbe8ca922441fae1e6eefa8
fots <facundotomatis@gmail.com>
2024-01-30 20:28:05 -0300

    added ratio of approved visits and principles views

--- spgunlp/app/src/main/java/com/example/spgunlp/ui/stats/StatsFragment.kt
+++ spgunlp/app/src/main/java/com/example/spgunlp/ui/stats/StatsFragment.kt
@@ -4,6 +4,7 @@ import android.animation.ObjectAnimator
 import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,9 +13,18 @@ import androidx.core.animation.doOnEnd
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.example.spgunlp.R
 import com.example.spgunlp.databinding.FragmentStatsBinding
+import com.example.spgunlp.io.VisitService
+import com.example.spgunlp.model.AppVisitParameters
 import com.example.spgunlp.ui.BaseFragment
+import com.example.spgunlp.util.PreferenceHelper
+import com.example.spgunlp.util.PreferenceHelper.get
+import com.example.spgunlp.util.getPrinciples
+import com.example.spgunlp.util.updatePreferencesPrinciple
+import kotlinx.coroutines.launch
+import java.util.Date
 
 class StatsFragment : BaseFragment() {
 
@@ -23,6 +33,9 @@ class StatsFragment : BaseFragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private val visitService: VisitService by lazy {
+        VisitService.create()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -44,6 +57,29 @@ class StatsFragment : BaseFragment() {
             ).apply {
                 duration = 3000
                 start()
+            }
+        }
+
+        lifecycleScope.launch {
+            val preferences = PreferenceHelper.defaultPrefs(requireContext())
+            val jwt = preferences["jwt", ""]
+            val header = "Bearer $jwt"
+            val principles: List<AppVisitParameters.Principle>
+
+            try {
+                val response = visitService.getPrinciples(header)
+                val body = response.body()
+                if (response.isSuccessful && body != null) {
+                    principles = body
+                    //viewModel.updatePrinciplesList(principles)
+                    Log.i("SPGUNLP_TAG", "getPrinciples: made api call and was successful")
+                    Log.i("SPGUNLP_TAG", "getPrinciples: ${principles.size}")
+                } else if (response.code() == 401 || response.code() == 403) {
+                    //principles = viewModel.getPrinciplesList()!!
+                }
+            } catch (e: Exception) {
+                Log.e("SPGUNLP_TAG", e.message.toString())
+                //principles = viewModel.getPrinciplesList()!!
             }
         }
         return root
