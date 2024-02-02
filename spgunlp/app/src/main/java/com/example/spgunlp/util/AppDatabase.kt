964c84c6744836629ebfab16e83ccbe97877e340
fots <facundotomatis@gmail.com>
2024-02-02 16:37:34 -0300

    added app messages as an entity, also added dao and repo

--- spgunlp/app/src/main/java/com/example/spgunlp/util/AppDatabase.kt
+++ spgunlp/app/src/main/java/com/example/spgunlp/util/AppDatabase.kt
@@ -4,23 +4,28 @@ import android.content.Context
 import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.spgunlp.daos.MessageDao
 import com.example.spgunlp.daos.PerfilDao
 import com.example.spgunlp.daos.PoligonoDao
+import com.example.spgunlp.model.AppMessage
 import com.example.spgunlp.model.Perfil
 import com.example.spgunlp.model.Poligono
 
 @Database(
-    entities = [Poligono::class, Perfil::class],
-    version = 3,
+    entities = [Poligono::class, Perfil::class,AppMessage::class],
+    version = 4,
     exportSchema = true,
     autoMigrations = [
         AutoMigration(from = 1, to = 2),
-        AutoMigration(from = 2, to = 3)
+        AutoMigration(from = 2, to = 3),
+        AutoMigration(from = 3, to = 4),
     ]
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun poligonoDao(): PoligonoDao
     abstract fun perfilDao(): PerfilDao
+
+    abstract fun  messageDao(): MessageDao
 
     companion object {
         @Volatile
